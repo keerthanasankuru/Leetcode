0ee0162e98a849d6934a83f52d900eaeac1afe13
keerthanasankuru <147173030+keerthanasankuru@users.noreply.github.com>
2026-04-14 00:00:04 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,16 @@
-#DAY!
+#Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
+
+#Return abs(i - start).
+
+#It is guaranteed that target exists in nums.
+
+
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        minn=float('inf')
+        for i in range(len(nums)):
+            
+            if nums[i]==target:
+                minn=min(minn,abs(i-start))
+                
+        return minn        
